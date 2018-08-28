858632800fe94df5a84a73f830b1f812064e1f1c
GYJhuifei <1325949581@qq.com>
2018-08-28 12:40:03 +0800

    答题页瀑布流添加

--- src/view/answer/answer.vue
+++ src/view/answer/answer.vue
@@ -4,19 +4,57 @@
       <router-link to="/answer/indexIndustry" class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></router-link>
       <h1 class="mui-title">正在答题</h1>
     </header>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
-      <p>answer页面</p>
+    <div id="answer-body" class="answer-body">
+        <full-page ref="fullpage" :options="options" id="fullpage">
+        <div class="section">
+          <a class="answer-title"><span>12.</span>计算机病毒的预防措施包括毒的的预防措施包括毒的的预防措施包括毒的预防措施包括毒的预防措施包括下列哪项</a> 
+						<ul class="answer-list">
+							<li class="active"><i class="iconfont icon-A"></i><a>123123123123123123123</a></li>
+							<li><i class="iconfont icon-B"></i><a>123123123123123123123123123123123123123123123123</a></li>
+							<li><i class="iconfont icon-C"></i><a>123123123123123123123123123123123123123123123123</a></li>
+							<li><i class="iconfont icon-D"></i><a>123123123123123123123123123123123123123123123123</a></li>
+						</ul>
+        </div>
+        <div class="section">
+          <a class="answer-title"><span>12.</span>计算机病毒的预防措施包括毒的的预防措施包括毒的的预防措施包括毒的预防措施包括毒的预防措施包括下列哪项</a> 
+						<ul class="answer-list">
+							<li class="active"><i class="iconfont icon-A"></i><a>123123123123123123123</a></li>
+							<li><i class="iconfont icon-B"></i><a>123123123123123123123123123123123123123123123123</a></li>
+							<li><i class="iconfont icon-C"></i><a>123123123123123123123123123123123123123123123123</a></li>
+							<li><i class="iconfont icon-D"></i><a>123123123123123123123123123123123123123123123123</a></li>
+						</ul>
+        </div>
+        <div class="section">
+          <a class="answer-title"><span>12.</span>计算机病毒的预防措施包括毒的的预防措施包括毒的的预防措施包括毒的预防措施包括毒的预防措施包括下列哪项</a> 
+						<ul class="answer-list">
+							<li class="active"><i class="iconfont icon-A"></i><a>123123123123123123123</a></li>
+							<li><i class="iconfont icon-B"></i><a>123123123123123123123123123123123123123123123123</a></li>
+							<li><i class="iconfont icon-C"></i><a>123123123123123123123123123123123123123123123123</a></li>
+							<li><i class="iconfont icon-D"></i><a>123123123123123123123123123123123123123123123123</a></li>
+						</ul>
+        </div>
+      </full-page>
+    </div>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3'],
+        // sectionsColor: ['#41b883', '#ff5f45', '#0798ec']
+      },
+      mounted() {
+        
+      },
+    }
+  }
+}
+</script>
+<style scoped src="@css/index/answer.css"></style>
 <style scoped>
   .indexContainer {
     padding-top: 44px;
