37b3b5921a9e29362ff19b9c4943bbf4c36e972e
MR-zn <846179626@qq.com>
2018-09-02 00:21:25 +0800

    完成积分兑换界面

--- src/view/integral.vue
+++ src/view/integral.vue
@@ -1,3 +1,58 @@
 <template>
-  <div>这是积分兑换</div>
+  <div>
+    <header class="header">
+      <a>积分兑换</a>
+    </header>
+
+    <div class="container">
+      <div class="box">
+        <div class="oCircle">
+          <div class="textBox">
+            <h3>我的积分</h3>
+            <h3 class="score">{{score}}</h3>
+          </div>
+        </div>
+        <div class="btnBox">
+          <el-row>
+            <el-button round class="go" @click="go">立即兑换</el-button>
+            <p>注：1000积分兑换1个学分</p>
+          </el-row>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
+
+<script>
+import { MessageBox } from 'mint-ui'
+
+export default {
+  data() {
+    return {
+      score: 567
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    go() {
+      MessageBox({
+        title: '提示',
+        message: '确认兑换吗？',
+        showCancelButton: true
+      })
+        .then(() => {
+          MessageBox.alert('兑换成功！', '恭喜')
+        })
+    }
+  }
+}
+</script>
+
+
+<style scoped src="@css/ranking/index.css">
+</style>
+
+<style scoped src="@css/integral.css">
+</style>
